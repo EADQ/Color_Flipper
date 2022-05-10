866c16d69765073183eef54e04e8dc55270f70c9
EADQ <easoftwareengineer@gmail.com>
2022-05-09 21:49:51 -0600

    Testing document.body on the console web browser

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-  const randomNumber =2;
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
-})
-
+  console.log(document.body);
+});
 
